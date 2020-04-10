f7e82e0a70a994be3a0b7ee581963d7464437668
Lavrentev Stanislav <hvoinii@gmail.com>
2020-04-10 21:04:38 +0600

    add vue code to form

--- client/frontend.js
+++ client/frontend.js
@@ -1,3 +1,25 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
-console.log(Vue);
+new Vue({
+	el: '#app',
+	data() {
+		return {
+			form: {
+				name: '',
+				value: ''
+			},
+			contacts : []
+		}
+	},
+	methods: {
+		createContact() {
+			const {...contact} = this.form;
+			
+			this.contacts.push({...contact, id: Date.now()});
+
+			this.form.name = '';
+			this.form.value = '';
+
+		}
+	}
+})
